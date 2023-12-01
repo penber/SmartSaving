9c1d01d6db42bd8203bfb5a29bebdf5b06eff1ee
raey <raey.assefa@heig-vd.ch>
2023-12-01 12:41:32 +0100

    retour en arrière sur la conncection db en local

--- app.js
+++ app.js
@@ -15,21 +15,21 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 
 dotenv.config();  // Charger les variables d'environnement
-const uri = process.env.MONGODBAtlas_URI;
+// const uri = process.env.MONGODBAtlas_URI;
 
 const app = express();
 
 app.use(express.json());  // Middleware pour lire le corps JSON
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // Parse the OpenAPI document.
 const openApiDocument = yaml.load(fs.readFileSync('./openapi.yml'));
 // Serve the Swagger UI documentation.
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 app.use('/apidoc', express.static(path.join(__dirname, 'docs')));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -39,46 +39,46 @@ app.get('/', (req, res) => {
 });
 
 
-// let dbUri;
+let dbUri;
 
-// if (process.env.NODE_ENV === 'test') {
-//   dbUri = process.env.TEST_MONGODB_URI;
-// } else {
-//   dbUri = process.env.MONGODB_URI;
-// }
-
-// await mongoose.connect(dbUri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log('MongoDB Connected');
-// }).catch(err => {
-// console.error('Failed to connect to MongoDB', err);
-// process.exit(1);
-// });
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+if (process.env.NODE_ENV === 'test') {
+  dbUri = process.env.TEST_MONGODB_URI;
+} else {
+  dbUri = process.env.MONGODB_URI;
 }
-run().catch(console.dir);
+
+await mongoose.connect(dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB Connected');
+}).catch(err => {
+console.error('Failed to connect to MongoDB', err);
+process.exit(1);
+});
+
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 // Routes
